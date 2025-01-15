b3cc1598b3c0683ec990211e689ee7a5f8cecceb
jmdfk <36947357+jmdfk@users.noreply.github.com>
2025-01-15 08:40:27 +0000

    Pending changes exported from your codespace

--- 02_helloworld/src/main.c
+++ 02_helloworld/src/main.c
@@ -5,8 +5,17 @@
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(log_demo, LOG_LEVEL_DBG);
+
 
 int main(void)
 {
-	printk("Hello World! %s\n", CONFIG_BOARD);
+	int counter = 0;
+	while(1) {
+		printk("Hello World! %s\n", CONFIG_BOARD);
+		//LOG_INF("Hello World! %d\n", counter);
+		++counter;
+		k_msleep(1000);
+	}
 }
